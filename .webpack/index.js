4ae4bbb43b47d119efef0f39c53e1255252b118f
jupl <juan.placencia.512@gmail.com>
2016-10-24 16:30:53 -0500

    Make it easier to change build paths for Webpack

--- .webpack/index.js
+++ .webpack/index.js
@@ -3,32 +3,49 @@ const addAssets = require('./assets')
 const addDevelopment = require('./development')
 const addProduction = require('./production')
 const addHot = require('./hot')
+const addStory = require('./story')
 
-// Create base configuration and export
-const config = module.exports = createConfig()
+/**
+ * Build Webpack configuration
+ * @param {Object} options - Options
+ * @param {string} options.source - Source path to read source code from
+ * @param {string} options.destination - Destination path to write assets out
+ * @param {string} options.assets - Assets path to read static assets from
+ * @return {Object} Webpack configuration
+ */
+module.exports = ({
+  source,
+  destination,
+  assets,
+}) => {
+  // Create base configuration and export
+  const config = createConfig({source, destination})
 
-// Add to configuration based on environment
-switch(process.env.NODE_ENV) {
-case 'development':
-case 'storybook':
-  addDevelopment(config)
-  console.info('--- webpack: using development configuration')
-  break
-case 'production':
-  addProduction(config)
-  console.info('--- webpack: using production configuration')
-  break
-default:
-  throw new Error('NODE_ENV is not set or is an unsupported environment')
-}
+  // Add to configuration based on environment
+  switch(process.env.NODE_ENV) {
+  case 'storybook':
+    addStory(config)
+    addDevelopment(config)
+    console.info('--- webpack: using storybook configuration')
+  case 'development':
+    addAssets(config, {assets})
+    addDevelopment(config)
+    console.info('--- webpack: using development configuration')
+    break
+  case 'production':
+    addAssets(config, {assets})
+    addProduction(config)
+    console.info('--- webpack: using production configuration')
+    break
+  default:
+    throw new Error('NODE_ENV is not set or is an unsupported environment')
+  }
 
-// Copy over assets except when running storybook
-if(process.env.NODE_ENV !== 'storybook') {
-  addAssets(config)
-}
+  // Add hot reload support if explicitly specified
+  if(process.env.HOT_MODULES === 'true') {
+    addHot(config)
+    console.info('--- webpack: using hot modules configuration')
+  }
 
-// Add hot reload support if explicitly specified
-if(process.env.HOT_MODULES === 'true') {
-  addHot(config)
-  console.info('--- webpack: using hot modules configuration')
+  return config
 }
