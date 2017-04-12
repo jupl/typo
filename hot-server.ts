c436ab09487d77dd409e99e8f04c7f071fe86ced
jupl <juan.placencia.512@gmail.com>
2017-04-11 21:38:43 -0500

    Updates and improvements

--- hot-server.ts
+++ hot-server.ts
@@ -1,27 +1,33 @@
 import {Server} from 'hapi'
 import * as webpack from 'webpack'
 import config from './webpack.config'
-
-// Create a new server instance
-const server = new Server()
+import * as hapiWebpack from 'hapi-webpack-plugin'
 
 // Set up server port number with a default
 const RADIX = 10
 const DEFAULT_PORT = 3000
 const port = parseInt(process.env.PORT, RADIX) || DEFAULT_PORT
-server.connection({port})
 
-// Add webpack plugin so server knows to get static assets from builds
-server.register({
-  register: require('hapi-webpack-plugin'),
-  options: {
-    compiler: webpack(config),
-    assets: {
-      noInfo: true,
-      publicPath: config.output && config.output.publicPath,
+initialize() // tslint:disable-line:no-floating-promises
+
+async function initialize() {
+  // Create a new server instance
+  const server = new Server()
+  server.connection({port})
+
+  // Add webpack plugin so server knows to get static assets from builds
+  await server.register({
+    register: hapiWebpack,
+    options: {
+      compiler: webpack(config),
+      assets: {
+        noInfo: true,
+        publicPath: config.output && config.output.publicPath,
+      },
     },
-  },
-}, error => { if(error) { throw error } })
+  })
 
-// Start server and notify
-server.start(() => console.log(`Server running at: ${server.info.uri}`))
+  // Start server and notify
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+}
