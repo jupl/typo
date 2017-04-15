45ea2c662e8b40754a01456c04565b0ae5d7d76a
jupl <juan.placencia.512@gmail.com>
2017-04-15 11:22:15 -0500

    Simplify async handler code

--- common/plugins/async-handler.ts
+++ common/plugins/async-handler.ts
@@ -5,18 +5,10 @@ import * as asyncHandler from 'hapi-es7-async-handler'
 @Plugin({name: 'hapi-async-handler', version: '1.0.3'})
 export default class AsyncHandlerPlugin implements IPlugin {
   public async register(server: Server, _options: {}, next: Function) {
-    let error
-    try {
-      await server.register({
-        register: asyncHandler,
-        options: {server},
-      })
-    }
-    catch(e) {
-      error = e
-    }
-    finally {
-      next(error)
-    }
+    await server.register({
+      register: asyncHandler,
+      options: {server},
+    })
+    next()
   }
 }
