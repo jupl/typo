a81ab503a0679190d3fbfdea6b677cc7b25c983b
jupl <juan.placencia.512@gmail.com>
2017-05-07 13:33:40 -0500

    s/bin/main

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,6 @@ import {addToEntries, createConfiguration} from 'wcb'
 // tslint:disable-next-line:no-default-export
 export default addToEntries(createConfiguration({
   log: message => console.log(message),
-  pattern: ['**/bin.ts'],
+  pattern: ['**/main.ts'],
   target: 'node',
 }), ['dotenv/config'])
