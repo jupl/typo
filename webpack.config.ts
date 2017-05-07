c234c6b37c71b9f0f0b70570a7e8af5cbed19307
jupl <juan.placencia.512@gmail.com>
2017-05-07 11:53:14 -0500

    Add common chunk plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,8 @@
 import {addRules, createConfiguration} from 'wcb'
+import {optimize} from 'webpack'
 
-// tslint:disable-next-line:no-default-export
-export default addRules(createConfiguration({
+// Build configuration
+let configuration = addRules(createConfiguration({
   assets: 'assets',
   destination: 'static',
   log: message => console.log(message),
@@ -10,3 +11,15 @@ export default addRules(createConfiguration({
   {test: /\.css$/, use: ['style-loader', 'css-loader']},
   {test: /\.(gif|jpg|jpeg|png|svg)$/, use: ['file-loader']},
 ])
+if(Object.keys(configuration.entry).length > 1) {
+  configuration = {
+    ...configuration,
+    plugins: [
+      ...configuration.plugins,
+      new optimize.CommonsChunkPlugin({name: 'common'}),
+    ],
+  }
+}
+
+// tslint:disable-next-line:no-default-export
+export default configuration
