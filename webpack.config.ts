5415ce16f929e8594aee877de6b5e84138538ec1
jupl <juan.placencia.512@gmail.com>
2017-05-07 10:44:06 -0500

    Add logging to wcb

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import {addToEntries, createConfiguration} from 'wcb'
 
 // tslint:disable-next-line:no-default-export
 export default addToEntries(createConfiguration({
+  log: console.log,
   pattern: ['**/bin.ts'],
   target: 'node',
 }), ['dotenv/config'])
