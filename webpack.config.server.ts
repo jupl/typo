461d1508def000933516f8ef6774a22f317825f8
jupl <juan.placencia.512@gmail.com>
2017-05-08 21:05:14 -0500

    Update wcb and add banner manually

--- webpack.config.server.ts
+++ webpack.config.server.ts
@@ -1,8 +1,22 @@
 import {addToEntries, createConfiguration} from 'wcb'
+import {BannerPlugin} from 'webpack'
 
 /** Webpack configuration to build server */
-export const configuration = addToEntries(createConfiguration({
+export let configuration = addToEntries(createConfiguration({
   log: message => console.log(`[server] ${message}`),
   pattern: ['**/main.ts'],
   target: 'node',
 }), ['dotenv/config'])
+
+// Add banner to bin files
+configuration = {
+  ...configuration,
+  plugins: [
+    ...configuration.plugins,
+    new BannerPlugin({
+      banner: '#!/usr/bin/env node',
+      entryOnly: true,
+      raw: true,
+    }),
+  ],
+}
