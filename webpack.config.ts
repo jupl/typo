efc13ba818d2f77403c9d9613323a6ae13ef9286
jupl <juan.placencia.512@gmail.com>
2017-05-08 21:03:31 -0500

    Update wcb and add banner manually

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,25 @@
 import {addToEntries, createConfiguration} from 'wcb'
+import {BannerPlugin} from 'webpack'
 
-// tslint:disable-next-line:no-default-export
-export default addToEntries(createConfiguration({
+// Build base configuration
+let configuration = addToEntries(createConfiguration({
   log: message => console.log(message),
   pattern: ['**/main.ts'],
   target: 'node',
 }), ['dotenv/config'])
+
+// Add banner to bin files
+configuration = {
+  ...configuration,
+  plugins: [
+    ...configuration.plugins,
+    new BannerPlugin({
+      banner: '#!/usr/bin/env node',
+      entryOnly: true,
+      raw: true,
+    }),
+  ],
+}
+
+// tslint:disable-next-line:no-default-export
+export default configuration
