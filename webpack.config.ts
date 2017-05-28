f594f7dec463b14bd3e8b8bb865b6c7964ae1a70
jupl <juan.placencia.512@gmail.com>
2017-05-28 15:23:16 -0500

    Update wcb

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,19 @@
-import {addToEntries, createConfiguration} from 'wcb'
+import {addPlugins, addToEntries, createConfiguration} from 'wcb'
 import {BannerPlugin} from 'webpack'
 
 // Build base configuration
-let configuration = addToEntries(createConfiguration({
+const configuration = addToEntries(addPlugins(createConfiguration({
   destination: 'dist/bin',
   log: message => console.log(message),
   source: 'src/bin',
   target: 'node',
-}), ['dotenv/config'])
-
-// Add banner to bin files
-configuration = {
-  ...configuration,
-  plugins: [
-    ...configuration.plugins,
-    new BannerPlugin({
-      banner: '#!/usr/bin/env node',
-      entryOnly: true,
-      raw: true,
-    }),
-  ],
-}
+}), [
+  new BannerPlugin({
+    banner: '#!/usr/bin/env node',
+    entryOnly: true,
+    raw: true,
+  }),
+]), ['dotenv/config'])
 
 // tslint:disable-next-line:no-default-export
 export default configuration
