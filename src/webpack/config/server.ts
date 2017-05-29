28ea247a071f656dcd031ed7bd9080128a7727fb
jupl <juan.placencia.512@gmail.com>
2017-05-29 01:06:51 -0500

    Use separate CSS file

--- src/webpack/config/server.ts
+++ src/webpack/config/server.ts
@@ -1,23 +1,18 @@
-import {addToEntries, createConfiguration} from 'wcb'
+import {addPlugins, addToEntries, createConfiguration} from 'wcb'
 import {BannerPlugin} from 'webpack'
+import {cssLoaders} from './common'
 
 /** Webpack configuration to build server */
-export let configuration = addToEntries(createConfiguration({
+export let configuration = addToEntries(addPlugins(createConfiguration({
+  cssLoaders,
   destination: 'dist/bin',
   log: message => console.log(`[server] ${message}`),
   source: 'src/bin',
   target: 'node',
-}), ['dotenv/config'])
-
-// Add banner to bin files
-configuration = {
-  ...configuration,
-  plugins: [
-    ...configuration.plugins,
-    new BannerPlugin({
-      banner: '#!/usr/bin/env node',
-      entryOnly: true,
-      raw: true,
-    }),
-  ],
-}
+}), [
+  new BannerPlugin({
+    banner: '#!/usr/bin/env node',
+    entryOnly: true,
+    raw: true,
+  }),
+]), ['dotenv/config'])
