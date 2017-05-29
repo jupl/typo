616723cc5135c6ff9514148d5f17d77ff44e44a5
jupl <juan.placencia.512@gmail.com>
2017-05-29 09:49:33 -0500

    s/let/const

--- src/webpack/config/client.ts
+++ src/webpack/config/client.ts
@@ -1,7 +1,7 @@
 import {createConfiguration} from 'wcb'
 
 // Build configuration
-export let configuration = createConfiguration({
+export const configuration = createConfiguration({
   assets: 'src/assets',
   common: true,
   cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
