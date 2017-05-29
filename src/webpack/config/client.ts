ddd35e6349986799643e777d3053bd19aa32aa64
jupl <juan.placencia.512@gmail.com>
2017-05-29 00:51:52 -0500

    Use separate CSS file

--- src/webpack/config/client.ts
+++ src/webpack/config/client.ts
@@ -1,23 +1,12 @@
-import {addRules, createConfiguration} from 'wcb'
-import {optimize} from 'webpack'
+import {createConfiguration} from 'wcb'
 
 // Build configuration
-export let configuration = addRules(createConfiguration({
+export let configuration = createConfiguration({
   assets: 'src/assets',
+  common: true,
+  cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
   destination: 'dist/assets',
   log: message => console.log(message),
   source: 'src/assets',
   useBabel: true,
-}), [
-  {test: /\.css$/, use: ['style-loader', 'css-loader']},
-  {test: /\.(gif|jpg|jpeg|png|svg)$/, use: ['file-loader']},
-])
-if(Object.keys(configuration.entry).length > 1) {
-  configuration = {
-    ...configuration,
-    plugins: [
-      ...configuration.plugins,
-      new optimize.CommonsChunkPlugin({name: 'common'}),
-    ],
-  }
-}
+})
