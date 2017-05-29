6711c9caf8c9ee1b597cbd19b495de8f3a4b36f2
jupl <juan.placencia.512@gmail.com>
2017-05-29 01:16:28 -0500

    Use separate CSS file

--- src/webpack/config/main.ts
+++ src/webpack/config/main.ts
@@ -1,7 +1,9 @@
 import {createConfiguration} from 'wcb'
+import {cssLoaders} from './common'
 
 /** Webpack configuration to build main process */
 export const configuration = createConfiguration({
+  cssLoaders,
   destination: 'dist/bin',
   log: message => console.log(`[main] ${message}`),
   source: 'src/bin',
--- src/webpack/config/renderer.ts
+++ src/webpack/config/renderer.ts
@@ -1,8 +1,10 @@
 import {addRules, createConfiguration} from 'wcb'
+import {cssLoaders} from './common'
 
 /** Webpack configuration to build renderer process */
 export const configuration = addRules(createConfiguration({
   assets: 'src/assets',
+  cssLoaders,
   destination: 'dist/assets',
   log: message => console.log(`[renderer] ${message}`),
   source: 'src/assets',
