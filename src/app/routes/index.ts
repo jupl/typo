b56807428bae9a4a2e5e385ec47ce555af604d10
jupl <juan.placencia.512@gmail.com>
2017-07-20 06:40:49 -0500

    Update and clean up dependencies

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,9 @@
 import {ReplyNoContinue as Reply, Request, Server} from 'hapi'
-import * as asyncHandler from 'hapi-es7-async-handler'
 
 Object.assign(register, {attributes: {name: 'app-routes'}})
 
 /** Add application routes to server instance */
 export async function register(server: Server, _: {}, next: Function) {
-  await server.register(asyncHandler, {once: true})
   server.route({
     method: 'GET',
     path: '/',
