dd756efaf47efe748c8b595082524eb0c169c189
jupl <juan.placencia.512@gmail.com>
2017-10-22 21:08:01 -0500

    Big update

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,8 +2,13 @@ import {ReplyNoContinue as Reply, Request, Server} from 'hapi'
 
 Object.assign(register, {attributes: {name: 'app-routes'}})
 
-/** Add application routes to server instance */
-export function register(server: Server, _: {}, next: Function) {
+/**
+ * Add application routes to server instance
+ * @param server Hapi server instance
+ * @param _options Unused options
+ * @param next Post calllback
+ */
+export function register(server: Server, _options: {}, next: Function) {
   server.route({
     method: 'GET',
     path: '/',
