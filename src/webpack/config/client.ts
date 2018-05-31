7ce30da77cd5d4cbe160e731e42095c1c85c4d2c
jupl <juan.placencia.512@gmail.com>
2018-05-31 09:25:01 -0500

    Updates

--- src/webpack/config/client.ts
+++ src/webpack/config/client.ts
@@ -3,10 +3,12 @@ import {createConfiguration} from 'wcb'
 /** Webpack configuration */
 export const configuration = createConfiguration({
   assets: 'src/assets',
+  atlOptions: {
+    useBabel: true,
+  },
   common: true,
   cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
   destination: 'dist/assets',
   log: message => console.log(message),
   source: 'src/assets',
-  useBabel: true,
 })
