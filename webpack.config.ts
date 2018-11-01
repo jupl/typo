bc84c07ce02afa21eed04eaacc562fed81ea17bf
jupl <juan.placencia.512@gmail.com>
2018-11-01 18:54:14 -0500

    Updates, updates, updates

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,1 +1,18 @@
-export {configuration as default} from './src/webpack/config/server'
+import {addPlugins, addToEntries, createConfiguration} from 'wcb'
+import {BannerPlugin} from 'webpack'
+
+const configuration = addToEntries(addPlugins(createConfiguration({
+  destination: 'dist/bin',
+  log: message => console.log(message),
+  source: 'src/bin',
+  target: 'node',
+}), [
+  new BannerPlugin({
+    banner: '#!/usr/bin/env node',
+    entryOnly: true,
+    raw: true,
+  }),
+]), ['dotenv/config'])
+
+/** Webpack config for server build */
+export default configuration // tslint:disable-line:no-default-export
