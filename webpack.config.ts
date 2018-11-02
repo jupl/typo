2ba6d6bfcb3a9b0b203b514c231277132ac23e4f
jupl <juan.placencia.512@gmail.com>
2018-11-01 22:25:15 -0500

    Update dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,1 +1,30 @@
-export {configuration as default} from './src/webpack/config/client'
+import {createConfiguration} from 'wcb'
+
+let configuration = createConfiguration({
+  assets: 'src/assets',
+  atlOptions: {
+    useBabel: true,
+  },
+  common: true,
+  cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
+  destination: 'dist/assets',
+  hotReload: process.env.HOT_MODULES === 'true' ? 'server' : 'none',
+  log: message => console.log(message),
+  source: 'src/assets',
+})
+
+configuration = {
+  ...configuration,
+  devServer: {
+    hot: process.env.HOT_MODULES === 'true',
+    port: 3000,
+    stats: {
+      all: false,
+      builtAt: true,
+      errors: true,
+    },
+  },
+}
+
+/** Webpack configuration */
+export default configuration // tslint:disable-line:no-default-export
