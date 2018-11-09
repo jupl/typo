0638e61c49217bbd461a9ec2361ca7af9bb24ae4
jupl <juan.placencia.512@gmail.com>
2018-11-08 19:06:24 -0600

    Update wcb

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import {createConfiguration} from 'wcb'
 
-let configuration = createConfiguration({
+// tslint:disable-next-line:no-default-export
+export default createConfiguration({
   assets: 'src/assets',
   atlOptions: {
     useBabel: true,
@@ -8,23 +9,7 @@ let configuration = createConfiguration({
   common: true,
   cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
   destination: 'dist/assets',
-  hotReload: process.env.HOT_MODULES === 'true' ? 'server' : 'none',
+  devServer: true,
   log: message => console.log(message),
   source: 'src/assets',
 })
-
-configuration = {
-  ...configuration,
-  devServer: {
-    hot: process.env.HOT_MODULES === 'true',
-    port: 3000,
-    stats: {
-      all: false,
-      builtAt: true,
-      errors: true,
-    },
-  },
-}
-
-/** Webpack configuration */
-export default configuration // tslint:disable-line:no-default-export
