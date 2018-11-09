b6c9c7dedd57f736c4c27436094ee196fcbb612f
jupl <juan.placencia.512@gmail.com>
2018-11-08 18:53:46 -0600

    Update wcb

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,8 @@
 import {addPlugins, addToEntries, createConfiguration} from 'wcb'
 import {BannerPlugin} from 'webpack'
 
-const configuration = addToEntries(addPlugins(createConfiguration({
+// tslint:disable-next-line:no-default-export
+export default addToEntries(addPlugins(createConfiguration({
   destination: 'dist/bin',
   log: message => console.log(message),
   source: 'src/bin',
@@ -13,6 +14,3 @@ const configuration = addToEntries(addPlugins(createConfiguration({
     raw: true,
   }),
 ]), ['dotenv/config'])
-
-/** Webpack config for server build */
-export default configuration // tslint:disable-line:no-default-export
