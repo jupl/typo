195ef5f415655051f8187a0df81a0657adec9fca
jupl <juan.placencia.512@gmail.com>
2018-11-23 01:25:56 -0600

    Use new wcb options

--- src/webpack/config/renderer.ts
+++ src/webpack/config/renderer.ts
@@ -3,9 +3,11 @@ import {createConfiguration} from 'wcb'
 /** Webpack configuration to build renderer process */
 export const configuration = createConfiguration({
   assets: true,
+  common: true,
   cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
   destination: 'dist/assets',
   devServer: true,
+  html: true,
   log: 'Renderer',
   source: 'src/assets',
   target: 'electron-renderer',
