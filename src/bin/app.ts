3d1c4e38948a0ceabfdd808226793ca261177a98
jupl <juan.placencia.512@gmail.com>
2018-12-26 00:08:48 -0600

    Logging and other enhancements

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -1,5 +1,6 @@
 import {Server} from 'hapi'
-import * as AppRoutes from '~/app/plugins/routes'
+import * as AppRoutes from '~/app/routes'
+import * as Log from '~/common/plugins/log'
 
 // Gather configuration data
 const production = process.env.NODE_ENV === 'production'
@@ -10,7 +11,10 @@ if(isNaN(port)) {
 
 (async() => { // tslint:disable-line:no-floating-promises
   const server = new Server({port, routes: {security: production}})
-  await server.register(AppRoutes.createPlugin())
+  await server.register([
+    Log.createPlugin(),
+    AppRoutes.createPlugin(),
+  ])
   await server.start()
-  console.log('Server running at:', server.info.uri)
+  server.log([], `Server running at ${server.info.uri}`)
 })()
